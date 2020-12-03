125916848e5f07d01f4a1e17bd713e4d9034b204
Ivan Sushkov <ivan.sushkov91@gmail.com>
2020-12-03 23:35:04 +0200

    Add: SMS sending service (all tests passed

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # api_01_sms
-API 01 sms
+SMS sending service. Uses the vk.com API and sends SMS if a friend becomes online. Created with Twilio service.
+
+#### Technical requirements
+All required packages are listed in ```requirements.txt```
+
+#### Application install and run
+1. Create virtualenv
+    ```bash
+    virtualenv venv
+    # if virtualenv doesn't exist - pip install virtualenv
+    ```
+2. Activate Virtualenv
+
+    ```bash
+    source venv/bin/activate
+    ```
+3. Install dependencies from ```requirements.txt```:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Author
+[Ivan Sushkov](https://github.com/ionesu/)
--- homework.py
+++ homework.py
@@ -1,27 +1,44 @@
-import time
+import os
 import requests
-
+import time
+from dotenv import load_dotenv
 from twilio.rest import Client
+
+load_dotenv()
+URL = 'https://api.vk.com/method/users.get'
+token = os.getenv('VK_TOKEN')
+V = 5.92
+account_sid = os.getenv('ACCOUNT_SID')
+auth_token = os.getenv('AUTH_TOKEN')
+CLIENT = Client(account_sid, auth_token)
+number_from = os.getenv('NUMBER_FROM')
+number_to = os.getenv('NUMBER_TO')
 
 
 def get_status(user_id):
     params = {
-        ...
+        'user_ids': user_id,
+        'access_token': token,
+        'v': V,
+        'fields': 'online'
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
+    response = requests.post(URL, params=params).json()['response']
+    return response[0]['online']
 
 
-def send_sms(sms_text, client):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+def sms_sender(sms_text):
+    message = CLIENT.messages.create(
+        body=sms_text,
+        from_=number_from,
+        to=number_to
+    )
+    return message.sid
 
 
-if __name__ == '__main__':
-    # тут происходит инициализация Client
-    vk_id = input('Введите id ')
+if __name__ == "__main__":
+    vk_id = input("Введите id ")
     while True:
         if get_status(vk_id) == 1:
-            send_sms(f'{vk_id} сейчас онлайн!')
+            sms_sender(f'{vk_id} сейчас онлайн!')
             break
         time.sleep(5)
